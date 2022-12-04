0b156721b0be7b1bb3e70325dbfbcf98d91a9f30
Yasaswini4 <111989708+Yasaswini4@users.noreply.github.com>
2022-12-04 18:35:58 -0500

    committing the repo files

--- src/main/java/in/co/health/care/mgt/sys/repository/MedicineRepository.java
+++ src/main/java/in/co/health/care/mgt/sys/repository/MedicineRepository.java
@@ -1,10 +1,19 @@
 package in.co.health.care.mgt.sys.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import in.co.health.care.mgt.sys.entity.MedicineEntity;
 
 public interface MedicineRepository extends JpaRepository<MedicineEntity, Long>{
 
 	public MedicineEntity findByName(String name);
+	
+	
+	@Query(nativeQuery = true,value = "select * from medicine where expiry_date<current_timestamp() and active=1")
+	public List<MedicineEntity> getExpiryDateRecord();
+	
+	
 }
